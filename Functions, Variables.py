b03a95f8bb40a403e5d124750d654e919837f25b
TehAutumnCore <naruto13581@gmail.com>
2023-10-17 09:31:45 -0700

    CS50P

---
+++ Functions, Variables.py
@@ -0,0 +1,7 @@
+#print("hello, world")    #print is a function, and ("hello, world") is an argument to the function
+
+# input("What's your name?")
+# print("hello, world")
+
+name = input("What's your name? ")
+print("hello " + name + "!")
